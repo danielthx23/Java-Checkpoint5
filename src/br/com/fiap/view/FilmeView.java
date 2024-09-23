0bae43babd8ce6385fff75e35e9e9972514e793c
Daniel Saburo Akiyama <125198871+danielthx23@users.noreply.github.com>
2024-09-23 19:19:41 -0400

    Feat: Fixed the directory

---
+++ src/br/com/fiap/view/FilmeView.java
@@ -0,0 +1,45 @@
+package br.com.fiap.view;
+
+import javax.swing.*;
+
+public class FilmeView {
+    public static void main(String[] args) {
+        int codigo;
+        String titulo, genero, produtora;
+        String[] escolha = {"Inserir","Alterar","Excluir","Listar"};
+        int opcao;
+        FilmeController filmeController = new FilmeController();
+        do {
+            try {
+                opcao = JOptionPane.showOptionDialog(null, "Escolha uma das opções abaixo para manipular um Cliente", "Escolha", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, escolha, escolha[0]);
+                switch (opcao) {
+                    case 0:
+                        titulo = JOptionPane.showInputDialog("Digite o titulo do filme");
+                        genero = JOptionPane.showInputDialog("Digite o genero do filme");
+                        produtora = JOptionPane.showInputDialog("Digite a produtora do filme");
+                        System.out.println(filmeController.inserirFilme(titulo, genero, produtora));
+                        break;
+                    case 1:
+                        codigo = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do filme"));
+                        titulo = JOptionPane.showInputDialog("Digite o NOVO titulo do filme");
+                        genero = JOptionPane.showInputDialog("Digite o NOVO genero do filme");
+                        produtora = JOptionPane.showInputDialog("Digite a NOVA produtora do filme");
+                        System.out.println(filmeController.alterarFilme(codigo, titulo, genero, produtora);
+                        break;
+                    case 2:
+                        codigo = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do filme"));
+                        System.out.println(filmeController.excluirFilme(codigo));
+                        break;
+                    case 3:
+                        JOptionPane.showMessageDialog(null, filmeController.listarTodosFilmes());
+                        break;
+                    default:
+                        System.out.println("Opção inválida!");
+                }
+            } catch (Exception e) {
+                System.out.println("Erro: " + e.getMessage());
+            }
+        } while (JOptionPane.showConfirmDialog(null, "Deseja continuar?", "Atenção", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == 0);
+        JOptionPane.showMessageDialog(null, "Fim de Programa");
+    }
+}
