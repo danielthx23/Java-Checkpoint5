59d6ce7f42faf8b5f158f0c6b998f2301eaad944
Daniel Saburo Akiyama <125198871+danielthx23@users.noreply.github.com>
2024-09-24 12:19:49 -0400

    Update FilmeView.java

--- src/br/com/fiap/view/FilmeView.java
+++ src/br/com/fiap/view/FilmeView.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 /**
  * @author Daniel Saburo Akiyama / RM: 558263
  * @author João Vitor Di Re / RM: 554749
- * @author Marcus Vila Nova da Silva / RM:
+ * @author Marcus Vila Nova / RM:
  * @author João Pedro R. / RM: 558199
  */
 public class FilmeView {
